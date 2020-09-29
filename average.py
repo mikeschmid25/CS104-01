695694b2ee207ab7c8d24e878b9706b940b50ce0
mikeschmid25 <71726189+mikeschmid25@users.noreply.github.com>
2020-09-29 14:38:52 -0400

    Before loop added comment

--- average.py
+++ average.py
@@ -5,8 +5,12 @@ average = 0.0
 scoreCount = 0
 
 numberofTests = int(input("Please enter the number of tests you want to average: "))
+
+#Make these next 3 lines repeat until scoreCount = numberofTests
 score = int(input("Please enter a score: "))
 scoreCount = scoreCount + 1
 total = total + score          
+
+
 average = total/scoreCount
 print("The average is ",average)
