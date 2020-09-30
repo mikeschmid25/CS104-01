a9d223346c591a5fb26f56d375c8d7d560b583e6
mikeschmid25 <71726189+mikeschmid25@users.noreply.github.com>
2020-09-30 19:29:13 -0400

    average program after while loop added

--- average.py
+++ average.py
@@ -3,14 +3,15 @@ score = 0
 total = 0
 average = 0.0
 scoreCount = 0
-
 numberofTests = int(input("Please enter the number of tests you want to average: "))
 
 #Make these next 3 lines repeat until scoreCount = numberofTests
-score = int(input("Please enter a score: "))
-scoreCount = scoreCount + 1
-total = total + score          
+while not (scoreCount == numberofTests):
+    score = int(input("Please enter a score: "))
+    scoreCount = (scoreCount + 1)
+    total = (total + score)
+    print(scoreCount)
+    print(total)
 
-
-average = total/scoreCount
+average = (total/scoreCount)
 print("The average is ",average)
