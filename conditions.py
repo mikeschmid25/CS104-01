22766e0c162215cb28aefd02fb5ef8de33662870
mikeschmid25 <71726189+mikeschmid25@users.noreply.github.com>
2020-10-07 21:50:16 -0400

    conditions assignment with loop

--- conditions.py
+++ conditions.py
@@ -1,12 +1,14 @@
 temp = float(input("Please enter the current temperature: "))
-if temp > 90:
-    print("Wear Shorts")
-elif temp > 70:
-    print("Short sleeves are fine")
-elif temp > 50:
-    print("Wear a jacket")
-elif temp > 32:
-    print("Wear a heavy coat")
-else:
-    print("Stay Inside")
+while temp < 999:
+    temp = float(input("Please enter the current temperature: "))
+    if temp > 90:
+        print("Wear Shorts")
+    elif temp > 70:
+        print("Short sleeves are fine")
+    elif temp > 50:
+        print("Wear a jacket")
+    elif temp > 32:
+        print("Wear a heavy coat")
+    else:
+        print("Stay Inside")
 
