128caf82a9715b7db2adefe05ae1b5e869b279a2
mikeschmid25 <71726189+mikeschmid25@users.noreply.github.com>
2020-11-24 10:45:16 -0500

    python queue simulation

---
+++ PythonQueue.py
@@ -0,0 +1,22 @@
+names = []
+names.append("John")
+names.append("Adam")
+names.append("Jessica")
+names.append("Dave")
+names.append("Bob")
+names.append("Rose")
+names.append("Alex")
+names.append("Neil")
+names.append("Chritina")
+names.append("Margaret")
+print(names)
+
+for x in names:
+    names.pop(0)
+    names.pop(0)
+    names.pop(0)
+    names.pop(0)
+    names.pop(0)
+print(names)
+    
+
